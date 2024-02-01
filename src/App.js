f9c32cd327c47a3d305eefdaeb556f2450c65709
Eduardo Morais <dudu.vgvm@gmail.com>
2024-02-01 15:28:59 -0300

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import styled from 'styled-components';
 import Header from './componentes/Header';
 
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+`
+
 function App() {
-  return (
-    <div className='App'>
-      <Header />
-    </div>
-  );
+    return (
+        <AppContainer>
+            <Header />
+        </AppContainer>
+    );
 }
 
 export default App;
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,16 +1,23 @@
-import './estilo.css';
 import logoImagem from '../../imagens/logo.svg';
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+
+const LogoImage = styled.img`margin-right: 12px;`
 
 function Logo() {
     return (
-		<div className='logo'>
-			<img
+		<LogoContainer>
+			<LogoImage
 			src={logoImagem}
 			alt='logo do projeto'
 			className='logo-img'
-			></img>
+			></LogoImage>
 			<p><strong>Alura</strong>Books</p>
-		</div>
+		</LogoContainer>
     )
 }
 
