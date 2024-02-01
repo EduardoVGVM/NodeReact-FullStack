7bf6b4a5419c1952177807cf06084254a204fc12
Eduardo Morais <dudu.vgvm@gmail.com>
2024-02-01 17:09:21 -0300

    Pesquisa e Estados nos componentes

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../imagens/livro.png'
+
+export const Livros = [
+    {nome: 'Liderança em Design', id:1, src:livro},
+    {nome: 'JavaScript Assertivo', id:2, src:livro},
+    {nome: 'O Famoso Diplomata', id:3, src:livro},
+    {nome: 'Uma Rebatida e um Erro', id:4, src:livro},
+]
