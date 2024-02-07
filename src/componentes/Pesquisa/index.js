65bc6b1f5754b59efef6163e06571a5240b1b9b4
Eduardo Morais <dudu.vgvm@gmail.com>
2024-02-07 16:07:35 -0300

    Projeto Finalizado

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -2,6 +2,7 @@ import Input from '../Input';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import { getLivros } from '../../servicos/livros';
+import { postFavorito } from '../../servicos/favoritos';
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -52,6 +53,11 @@ function Pesquisa() {
         setLivros(livrosAPI);
     }
 
+    async function insertFavorito(id) {
+        await postFavorito(id)
+        alert(`Livro de id:${id} inserido!`)
+}
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
@@ -65,7 +71,7 @@ function Pesquisa() {
                 }}
             />
             <p>{livrosPesquisados.map(livro => (
-                <Resultado>
+                <Resultado onClick={() => insertFavorito(livro.id)}>
                     <p>{livro.nome}</p>
                     <img src={livro.src} alt='capa do livro' />
                 </Resultado>
